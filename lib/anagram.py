a9d75f8c2c64d243b41372d6b93ca38c9e55789c
gitonga-david <gitonga.muriithi8@gmail.com>
2023-07-25 14:57:16 +0300

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagrams):
+        list_anagrams = []
+        for anagram in anagrams:
+            if sorted(anagram) == sorted(self.word):
+                list_anagrams.append(anagram)
+        return list_anagrams
